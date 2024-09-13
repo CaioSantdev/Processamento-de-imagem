57bf789f6f16d099082696f262b9044e6429c010
CaioSantDev <c.santosggwp@gmail.com>
2024-09-13 04:01:34 -0300

    trabalho

---
+++ operacoes.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+
+def adicao(imagem01, imagem02):
+    imagem1 = cv2.imread(imagem01)
+    imagem2 = cv2.imread(imagem02)
+
+    largura = max(imagem1.shape[0], imagem2.shape[0])
+    altura = max(imagem1.shape[1], imagem2.shape[1])
+
+    novaImagem = np.zeros((largura, altura, 3), dtype=np.uint8)
+
+    for x in range(largura):
+        for y in range(altura):
+            # Verifica se a coordenada (x, y) está dentro das dimensões da primeira imagem.
+            if x < imagem1.shape[0] and y < imagem1.shape[1]:
+                # Se estiver dentro das dimensões da primeira imagem, adiciona o valor do pixel correspondente na primeira imagem à nova imagem.
+                novaImagem[x, y] += imagem1[x, y]
+            
+            # Verifica se a coordenada (x, y) está dentro das dimensões da segunda imagem.
+            if x < imagem2.shape[0] and y < imagem2.shape[1]:
+                # Se estiver dentro das dimensões da segunda imagem, adiciona o valor do pixel correspondente na segunda imagem à nova imagem.
+                novaImagem[x, y] += imagem2[x, y]
+
+    cv2.imwrite('resultadoSoma.png', novaImagem)
+
+def subtracao(imagem01, imagem02):
+    imagem1 = cv2.imread(imagem01)
+    imagem2 = cv2.imread(imagem02)
+
+    largura = max(imagem1.shape[0], imagem2.shape[0])
+    altura = max(imagem1.shape[1], imagem2.shape[1])
+
+    novaImagem = np.zeros((largura, altura, 3), dtype=np.uint8)
+
+    for x in range(largura):
+        for y in range(altura):
+            # Verifica se a coordenada (x, y) está dentro das dimensões da primeira imagem.
+            if x < imagem1.shape[0] and y < imagem1.shape[1]:
+                # Se estiver dentro das dimensões da primeira imagem, adiciona o valor do pixel correspondente na primeira imagem à nova imagem.
+                novaImagem[x, y] += imagem1[x, y]
+            
+            # Verifica se a coordenada (x, y) está dentro das dimensões da segunda imagem.
+            if x < imagem2.shape[0] and y < imagem2.shape[1]:
+                # Se estiver dentro das dimensões da segunda imagem, subtrai o valor do pixel correspondente na segunda imagem da nova imagem.
+                novaImagem[x, y] -= imagem2[x, y]
+
+    cv2.imwrite('resultadoSubtracao.png', novaImagem)
+
+adicao('./imagens/image.png', './imagens/imgBinaria.png')
+subtracao('./imagens/image.png', './imagens/imgBinaria.png')
---
+++ reflexao.py
@@ -0,0 +1,30 @@
+import cv2
+
+def reflexaoHorizontal(imagem):
+    imagem = cv2.imread(imagem)
+    altura, largura, _ = imagem.shape
+    imagemReflexao = imagem.copy()
+
+    for y in range(altura):
+        for x in range(largura // 2):
+            temp = imagemReflexao[y, x].copy()  # Pixel temporário para armazenar o valor original
+            imagemReflexao[y, x] = imagemReflexao[y, largura - 1 - x]  # Troca o pixel atual com o correspondente na outra extremidade
+            imagemReflexao[y, largura - 1 - x] = temp  # Atualiza o pixel correspondente na outra extremidade com o valor original
+
+    cv2.imwrite('imagemReflexaoHorizontal.png', imagemReflexao)
+
+def reflexaoVertical(imagem):
+    imagem = cv2.imread(imagem)
+    altura, largura, _ = imagem.shape
+    imagemReflexao = imagem.copy()
+
+    for x in range(largura):
+        for y in range(altura // 2):
+            temp = imagemReflexao[y, x].copy()  # Pixel temporário para armazenar o valor original
+            imagemReflexao[y, x] = imagemReflexao[altura - 1 - y, x]  # Troca o pixel atual com o correspondente na outra extremidade
+            imagemReflexao[altura - 1 - y, x] = temp  # Atualiza o pixel correspondente na outra extremidade com o valor original
+    
+    cv2.imwrite('imagemReflexaoVertical.png', imagemReflexao)
+
+reflexaoHorizontal('./imagens/image.png')
+reflexaoVertical('./imagens/image.png')
